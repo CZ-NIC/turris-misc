ba8955aab4f67ede80409437acc4142001dcd050
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-08-19 16:43:51 +0200

    Disable the ssl checking for sending logs

--- logsend/logsend.sh
+++ logsend/logsend.sh
@@ -5,7 +5,7 @@
 # List of daemon names. Separate by \|, it's put into the regular expression.
 DAEMONS='ucollect\|updater\|watchdog'
 # Where to put the logs (don't forget the question mark at the end)
-BASEURL='https://test.securt.cz/logsend/upload.cgi?'
+BASEURL='https://securt-test.labs.nic.cz/logsend/upload.cgi?'
 RID="$(atsha204cmd serial-number)"
 CERT="/etc/ssl/startcom-cznic.pem"
 
@@ -13,4 +13,4 @@ CERT="/etc/ssl/startcom-cznic.pem"
 logread | \
 	/usr/bin/whatsnew /tmp/logs.last.sha1 | \
 	grep "^[^ ][^ ]*  *[0-9][0-9]*  *[0-9:][0-9:]* [^ ][^ ]* [a-z][a-z]*\.[a-z][a-z]* \($DAEMONS\)\(\[[0-9]*\]\|\):" | \
-	curl --cacert "$CERT" -T - "$BASEURL$RID" -X POST
+	curl -k --cacert "$CERT" -T - "$BASEURL$RID" -X POST
