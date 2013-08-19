b7bbf4e0a95030f989418565a09bc78ecbb17b6a
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-08-19 14:55:59 +0200

    The log sender to use HTTPS

--- logsend/logsend.sh
+++ logsend/logsend.sh
@@ -5,11 +5,12 @@
 # List of daemon names. Separate by \|, it's put into the regular expression.
 DAEMONS='ucollect\|updater\|watchdog'
 # Where to put the logs (don't forget the question mark at the end)
-BASEURL='http://securt-test.labs.nic.cz/logsend/upload.cgi?'
+BASEURL='https://test.securt.cz/logsend/upload.cgi?'
 RID="$(atsha204cmd serial-number)"
+CERT="/etc/ssl/startcom-cznic.pem"
 
 # Grep regexp: Month date time hostname something.something daemon
 logread | \
 	/usr/bin/whatsnew /tmp/logs.last.sha1 | \
 	grep "^[^ ][^ ]*  *[0-9][0-9]*  *[0-9:][0-9:]* [^ ][^ ]* [a-z][a-z]*\.[a-z][a-z]* \($DAEMONS\)\(\[[0-9]*\]\|\):" | \
-	curl -T - "$BASEURL$RID" -X POST
+	curl --cacert "$CERT" -T - "$BASEURL$RID" -X POST
