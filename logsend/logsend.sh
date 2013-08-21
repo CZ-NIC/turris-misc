e99941ca0dad6194227a9eacb3b08c11566eb87e
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-08-21 12:42:57 +0200

    Wait a random time
    With the cron-synchronized scripts, if they do some communication with
    our servers, do a random-time sleep, so they don't rush in all at once.

--- logsend/logsend.sh
+++ logsend/logsend.sh
@@ -9,6 +9,11 @@ BASEURL='https://securt-test.labs.nic.cz/logsend/upload.cgi?'
 RID="$(atsha204cmd serial-number)"
 CERT="/etc/ssl/startcom-cznic.pem"
 
+# Don't load the server all at once. With NTP-synchronized time, and
+# thousand clients, it would make spikes on the CPU graph and that's not
+# nice.
+sleep $(( $(tr -cd 0-9 </dev/urandom | head -c 8) % 120 ))
+
 # Grep regexp: Month date time hostname something.something daemon
 logread | \
 	/usr/bin/whatsnew /tmp/logs.last.sha1 | \
