53d55bd98fd623caf602063d9dd6918ca3eb30fa
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-10-25 09:01:52 +0200

    Send at most 10k lines of log
    If we produce huge amount of logs, take only the last 10 000 of them, to
    save the bandwidth.

--- logsend/logsend.sh
+++ logsend/logsend.sh
@@ -19,11 +19,13 @@ sleep $(( $(tr -cd 0-9 </dev/urandom | head -c 8) % 120 ))
 
 cp /tmp/logs.last.sha1 "$TMPFILE" || true
 # Grep regexp: Month date time hostname daemon
+# tail ‒ limit the size of upload
 (
 	cat /var/log/messages.1 || true
 	cat /var/log/messages
 ) | \
 	/usr/bin/whatsnew /tmp/logs.last.sha1 | \
 	grep "^[^ ][^ ]*  *[0-9][0-9]*  *[0-9:][0-9:]* [^ ][^ ]*  *\($DAEMONS\)\(\[[0-9]*\]\|\):" | \
+	tail -n 10000 \
 	curl --compress --cacert "$CERT" -T - "$BASEURL$RID" -X POST
 mv "$TMPFILE" /tmp/logs.last.sha1
