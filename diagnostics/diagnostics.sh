13080d5b8bcd099fbcafa3fbd044091ebd9c2588
Stepan Henek <stepan.henek@nic.cz>
2016-10-05 11:00:58 +0200

    diagnostics: cmdline options added (-O <directory>)
        * -O <directory> stores output into <directory>.preparing/<module_name>.out.preparing
          when the module finishes <directory>.preparing/<module_name>.out.preparing
          is renamed to <directory>.preparing/<module_name>.out. When all
          modules are finished <directory>.preparing is renamed to <directory>

--- diagnostics/diagnostics.sh
+++ diagnostics/diagnostics.sh
@@ -35,9 +35,10 @@ module_help() {
 }
 
 print_help() {
-	echo "$(basename $0) [-b] [-o <file>] [module1[ module2[...]]]"
+	echo "$(basename $0) [-b] [-o <file> | -O <file>] [module1[ module2[...]]]"
 	echo "	-b		run in background"
 	echo "	-o <file>	print output to a file"
+	echo "	-O <file>	print output to a directory module per file"
 	echo
 	echo modules:
 	module_help
@@ -45,7 +46,10 @@ print_help() {
 
 module_run() {
 	local module="$1"
-	if [ -n "$OUTPUT_FILE" ] ; then
+	if [ -n "$OUTPUT_DIRECTORY" ] ; then
+		./modules/"$module".module run 2>&1 | tail -n "$MAX_LINES_PER_MODULE" >> "$OUTPUT_DIRECTORY.preparing/$module".out.preparing
+		mv "$OUTPUT_DIRECTORY.preparing/$module".out.preparing "$OUTPUT_DIRECTORY.preparing/$module".out
+	elif [ -n "$OUTPUT_FILE" ] ; then
 		printf "############## %s\n" $module >> "$OUTPUT_FILE".preparing
 		./modules/"$module".module run 2>&1 | tail -n "$MAX_LINES_PER_MODULE" >> "$OUTPUT_FILE".preparing
 		printf "************** %s\n" $module >> "$OUTPUT_FILE".preparing
@@ -71,6 +75,14 @@ if [ "$1" = "-o" ] ; then
 	shift
 fi
 
+if [ "$1" = "-O" ] ; then
+	shift
+	OUTPUT_DIRECTORY="$1"
+	rm -rf "$OUTPUT_DIRECTORY" "$OUTPUT_DIRECTORY.preparing"
+	mkdir -p "$OUTPUT_DIRECTORY.preparing" || ( echo "Failed to created the log directory" && exit 1 )
+	shift
+fi
+
 if [ "$1" = help ] ; then
 	print_help
 	exit 0
@@ -91,7 +103,12 @@ for module in $modules_to_run ; do
 	fi
 done
 
-# rename the output file when finished
-if [ -n "$OUTPUT_FILE" ] ; then
-	mv "$OUTPUT_FILE".preparing "$OUTPUT_FILE"
+# rename the output directory when finished
+if [ -n "$OUTPUT_DIRECTORY" ] ; then
+	mv "$OUTPUT_DIRECTORY".preparing "$OUTPUT_DIRECTORY"
+else
+	# rename the output file when finished
+	if [ -n "$OUTPUT_FILE" ] ; then
+		mv "$OUTPUT_FILE".preparing "$OUTPUT_FILE"
+	fi
 fi
