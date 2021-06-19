CZ-NIC/turris-misc

commit 4f6a4385b0b36e2a3bbd9b95147b33e6010750b9

Update openSUSE Leep to 15.3 for aarch64
It seems that for armv7l is not ready yet. As there is only empty
content of the folder while for previous version it works. For now,
let's update it just for aarch64.

lxc-generator/generate-lxc.sh
@@ -102,7 +102,7 @@ add_image "Debian" "Buster" "aarch64" "`get_lxc_url debian/buster/arm64`"
 add_image "Fedora" "34" "aarch64" "`get_lxc_url fedora/34/arm64`"
 add_image "Gentoo" "stable" "armv7l" "`get_gentoo_url arm armv7a_hardfp`"
 add_image "openSUSE" "15.2" "armv7l" "`get_opensuse_url https://download.opensuse.org/ports/armv7hl/distribution/leap/15.2/appliances`"
-add_image "openSUSE" "15.2" "aarch64" "`get_opensuse_url https://download.opensuse.org/ports/aarch64/distribution/leap/15.2/appliances`"
+add_image "openSUSE" "15.3" "aarch64" "`get_opensuse_url https://download.opensuse.org/ports/aarch64/distribution/leap/15.3/appliances`"
 add_image "openSUSE" "Tumbleweed" "armv7l" "https://download.opensuse.org/ports/armv7hl/factory/appliances/opensuse-tumbleweed-image.armv7l-lxc.tar.xz"
 add_image "openSUSE" "Tumbleweed" "aarch64" "https://download.opensuse.org/ports/aarch64/tumbleweed/appliances/opensuse-tumbleweed-image.aarch64-lxc.tar.xz"
 add_image "Ubuntu" "Bionic" "armv7l" "`get_lxc_url ubuntu/bionic/armhf`"

commit 38728732199f3fcaa124874d99d4324c3e1f49d8

Update openSUSE Leap to 15.3 for armv7l
It seems that tarballs are finally there for armv7l, thus it
Fixes: 4f6a4385b0b36e2a3bbd9b95147b33e6010750b9 ("Update openSUSE Leep
to 15.3 for aarch64")

lxc-generator/generate-lxc.sh
@@ -101,7 +101,7 @@ add_image "Debian" "Buster" "armv7l" "`get_lxc_url debian/buster/armhf`"
 add_image "Debian" "Buster" "aarch64" "`get_lxc_url debian/buster/arm64`"
 add_image "Fedora" "34" "aarch64" "`get_lxc_url fedora/34/arm64`"
 add_image "Gentoo" "stable" "armv7l" "`get_gentoo_url arm armv7a_hardfp`"
-add_image "openSUSE" "15.2" "armv7l" "`get_opensuse_url https://download.opensuse.org/ports/armv7hl/distribution/leap/15.2/appliances`"
+add_image "openSUSE" "15.3" "armv7l" "`get_opensuse_url https://download.opensuse.org/ports/armv7hl/distribution/leap/15.3/appliances`"
 add_image "openSUSE" "15.3" "aarch64" "`get_opensuse_url https://download.opensuse.org/ports/aarch64/distribution/leap/15.3/appliances`"
 add_image "openSUSE" "Tumbleweed" "armv7l" "https://download.opensuse.org/ports/armv7hl/factory/appliances/opensuse-tumbleweed-image.armv7l-lxc.tar.xz"
 add_image "openSUSE" "Tumbleweed" "aarch64" "https://download.opensuse.org/ports/aarch64/tumbleweed/appliances/opensuse-tumbleweed-image.aarch64-lxc.tar.xz"